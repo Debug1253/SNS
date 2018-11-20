e00333d4f4191ecc77b525f49fe4e1e7ac9b565a
Debug1253 <debug1253@gmail.com>
2018-11-21 00:14:51 +0900

    [CLIENT] 더미 스테이지선택 화면 작성

--- Assets/Scripts/Common/SceneNameModel.cs
+++ Assets/Scripts/Common/SceneNameModel.cs
@@ -3,4 +3,5 @@
     public const string Title = "Title";
     public const string MainMenu = "MainMenu";
     public const string StageSelect = "StageSelect";
+    public const string Game = "Game";
 }
