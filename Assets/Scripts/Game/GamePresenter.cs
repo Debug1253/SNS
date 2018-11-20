4f0e8b200e81c117faeab94be70918732ca27934
Debug1253 <debug1253@gmail.com>
2018-11-21 00:34:47 +0900

    [CLIENT] 더미 게임 화면 작성

---
+++ Assets/Scripts/Game/GamePresenter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using UniRx;
+
+[RequireComponent(typeof(GameView))]
+public class GamePresenter : MonoBehaviour
+{
+    private GameView View { get { return GetComponent<GameView>(); } }
+
+	void Start ()
+    {
+        InitEventHandler();
+    }
+	
+    private void InitEventHandler()
+    {
+        View.OnGoToTitle.Subscribe(_ => LoadTitle()).AddTo(this);
+    }
+
+    private void LoadTitle()
+    {
+        // NOTE: 씬관리하는 클래스를 작성하고 싶다.
+        SceneManager.LoadScene(SceneNameModel.Title);
+    }
+}
