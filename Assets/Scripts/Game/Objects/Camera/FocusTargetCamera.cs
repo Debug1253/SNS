b13b2d09010bedc233ab81eb8305d4db647f25f1
Debug1253 <debug1253@gmail.com>
2018-12-03 21:59:21 +0900

    [CLIENT] 카메라 움직임 구현

---
+++ Assets/Scripts/Game/Objects/Camera/FocusTargetCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace sns.ActionCamera
+{
+    public class FocusTargetCamera : ICamera
+    {
+        private const float FOCUS_TARGET_SCREEN_SIZE = 500;
+
+        public float ScreenSize { get; private set; }
+
+        public FocusTargetCamera()
+        {
+            ScreenSize = FOCUS_TARGET_SCREEN_SIZE;
+        }
+
+        public void UpdatePosition(Transform cameraTransform, Vector3 targetPos)
+        {
+            cameraTransform.position = targetPos + Vector3.back;
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Objects/Camera/ICamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace sns.ActionCamera
+{
+    public interface ICamera
+    {
+        float ScreenSize { get; }
+
+        void UpdatePosition(Transform cameraTransform, Vector3 targetPos);
+    }
+}
