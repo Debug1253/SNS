76cac810930158f319deb75bcd81f59feff3cd51
debug <debug1253@gmail.com>
2018-12-13 18:41:59 +0900

    [CLIENT] 터치 스틱으로 캐릭터를 이동

--- Assets/Scripts/Game/Control/TouchControl.cs
+++ Assets/Scripts/Game/Control/TouchControl.cs
@@ -2,6 +2,7 @@
 
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 using sns.Path;
 
@@ -9,13 +10,53 @@ using UniRx;
 
 namespace sns.Control
 {
-    public class TouchControl : MonoBehaviour, IControl
+    public class TouchControl : MonoBehaviour, IControl, IDragHandler, IEndDragHandler
     {
         [SerializeField] private Button jumpButton;
-        //[SerializeField] private Button moveStick;
+        [SerializeField] private RectTransform bg;
+        [SerializeField] private Transform stick;
 
-        public IObservable<float> OnHorizontal() { return Observable.Return(0f); }
-        public IObservable<float> OnVertical() { return Observable.Return(0f); }
+        public IObservable<float> OnHorizontal() { return onHorizontal; }
+        public IObservable<float> OnVertical() { return onVertical; }
         public IObservable<bool> OnJump() { return jumpButton.OnClickAsObservable().Select(_ => true); }
-    }
+
+        private Subject<float> onHorizontal = new Subject<float>();
+        private Subject<float> onVertical = new Subject<float>();
+
+        private Vector2 stickDefaultPos;
+        private Vector2 stickDir;
+        private float radius;
+
+		private void Start()
+		{
+            Initialize();
+		}
+
+        private void Initialize()
+        {
+            radius = bg.sizeDelta.y * 0.5f;
+            stickDefaultPos = stick.localPosition;
+        }
+
+        public virtual void OnDrag(PointerEventData data)
+        {
+            stickDir = (data.position - stickDefaultPos).normalized;
+
+            var distance = Vector3.Distance(data.position, stickDefaultPos);
+            var pos = stickDir * Mathf.Min(distance, radius);
+            stick.localPosition = stickDefaultPos + pos;
+
+            onHorizontal.OnNext(pos.x / radius);
+            onVertical.OnNext(pos.y / radius);
+        }
+
+        public virtual void OnEndDrag(PointerEventData data)
+        {
+            stick.localPosition = stickDefaultPos;
+            stickDir = Vector2.zero;
+
+            onHorizontal.OnNext(0f);
+            onVertical.OnNext(0f);
+        }
+	}
 }
